3332932bc15dab7c45218eb105cbbeaaf5ec3638
Leonardo Rosas <leonrosant92@gmail.com>
2023-08-16 16:39:04 -0600

    :tada: First commit
    Se agrega configuracion inicial del proyecto junto con el router para navegacion entre las paginas

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+
+const isGitHubPages = true;
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? "" : "/";
+
+export default defineConfig({
+  root: "src",
+  base,
+  mode,
+  envDir: "../",
+  publicDir: "../public",
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    }
+  },
+  build: {
+    outDir: "../dist",
+    assetsDir: "./"
+  }
+});
